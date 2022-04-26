bd135d1ee9ba28c6c7a94497e8d3e487157530c8
OmidrezaBagherian <OmidCoder21@gmail.com>
2022-04-26 11:48:44 -0700

    Start project and add viewBinding and dataBinding

---
+++ app/src/main/java/ir/omidrezabagherian/todoapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package ir.omidrezabagherian.todoapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.omidrezabagherian.todoapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
+    }
+}
